c8f47fcfc56cb8dbc1e06dc1ae32530deab0cc8e
Tan Teng Xing <seventan1234@gmail.com>
2023-12-01 19:58:18 +0800

    fix: pages, navi. add favor item

--- wp-content/plugins/google-listings-and-ads/js/build/gtag-events.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => '67fb0e5a4124c10b0fde');
--- wp-content/plugins/google-listings-and-ads/js/build/product-attributes.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '84b63622e2e6eceb4814');
